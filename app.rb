d2d5b9c2e37c6645333324d95e96aa7cd1f5821b
kieraw <wheatl31@msu.edu>
2015-02-25 12:07:34 -0500

    fixed again

--- app.rb
+++ app.rb
@@ -5,9 +5,9 @@ require 'holidapi'
 class HolidayApp < Sinatra::Base
   get '/' do
     params['country'] ||= 'us'
-	params['year'] ||= 2015
-	params['month'] ||= 1
-   	@holiday = HolidApi.get(country: params['country'], year: params['year'], month: params['month'])
+    params['year'] ||= 2015
+    params['month'] ||= 1
+    @holiday = HolidApi.get(country: params['country'], year: params['year'], month: params['month'])
     erb :holidays
   end
 end
