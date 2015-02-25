c26917dd979a4ad520928bae6b6a8c97edd1d913
kieraw <wheatl31@msu.edu>
2015-02-25 10:55:47 -0500

    fixed indendation

--- app.rb
+++ app.rb
@@ -3,11 +3,11 @@ require 'sinatra'
 require 'holidapi'
 
 class HolidayApp < Sinatra::Base
-	get '/' do
-		params['country'] ||= 'us'
-		params['year'] ||= 2015
-		params['month'] ||= 1
-   		@holiday = HolidApi.get(country: params['country'], year: params['year'], month: params['month'])
-    	erb :holidays
-    end
+  get '/' do
+    params['country'] ||= 'us'
+	params['year'] ||= 2015
+	params['month'] ||= 1
+   	@holiday = HolidApi.get(country: params['country'], year: params['year'], month: params['month'])
+    erb :holidays
+  end
 end
